9317c3fd2bac4a967349be6bea6a56bb52447d0b
Kristina Zvonar <kristinaa.zvonar@hotmail.com>
2018-11-01 23:29:13 +0100

    Some corrections

--- WeatherServiceDemo/WebContent/resources/js/init.js
+++ WeatherServiceDemo/WebContent/resources/js/init.js
@@ -19,7 +19,36 @@ $(document).ready(function() {
 
 	$('#destinationButton').click(function() {
 		var latitude = $('#destinationLatitude').val();
-		var longitude = $('#destinationLatitude').val();
+		var longitude = $('#destinationLongitude').val();
 		getWeatherData(latitude, longitude, 'destination');
 	});
+	
+	$('#departureAddress').on('input', function(){
+		$('#departureLongitude, #departureLatitude').prop('disabled', this.value !== '');
+	});
+	
+	$('#destinationAddress').on('input', function(){
+		$('#destinationLatitude, #destinationLongitude').prop('disabled', this.value !== '');
+	});
+	
+	$('#departureLatitude, #departureLongitude').on('input', function(){
+		$('#departureAddress, #departureGeocode').prop('disabled', this.value !== '');
+		$('#departureButton').prop('disabled', this.value === '');
+	});
+	
+	$('#destinationLatitude, #destinationLongitude').on('input', function(){
+		$('#destinationAddress, #destinationGeocode').prop('disabled', this.value !== '');
+		$('#destinationButton').prop('disabled', this.value === '');
+	});
+	
+	$('#departureButton').prop('disabled', true);
+	$('#departureGeocode').click(function(){
+		$('#departureButton').prop('disabled', false);
+	});
+	
+	$('#destinationButton').prop('disabled', true);
+	$('#destinationGeocode').click(function(){
+		$('#destinationButton').prop('disabled', false);
+	});
+	
 });
--- WeatherServiceDemo/WebContent/resources/js/util.js
+++ WeatherServiceDemo/WebContent/resources/js/util.js
@@ -13,3 +13,27 @@ const SERVICE = {
 		VALUE: 'value',
 		PERCENT: 'percent'
 }
+
+const LAYOUT = {
+		DEW_POINT : 'DewPointValue',
+		HUMIDITY : 'HumidityValue',
+		TEMPERATURE : 'TemperatureValue',
+		FOG : "Fog",
+		LOW_CLOUDS : 'LowClouds',
+		MEDIUM_CLOUDS : 'MediumClouds',
+		HIGH_CLOUDS : 'HighClouds',
+		DEPARTURE : 'departure',
+		DESTINATION : 'destination',
+		ID: '#',
+		PERCENT: '%',
+		FORM: 'Form',
+		IMAGE: 'Img',
+		ADDRESS: 'Address',
+		LATITUDE: 'Latitude',
+		LONGITUDE: 'Longitude'
+	}
+
+const ERROR_MESSAGE = {
+		REQUEST_FAILURE: 'There was an error fetching data from the service. Please try again.',
+		INVALID_COORDINATES: 'Invalid coordinates provided',	
+}
